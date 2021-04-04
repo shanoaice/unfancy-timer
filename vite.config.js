6ac445784d03b7011b1e945faaa472ebdde629a5
Shanoa Ice <25174488+shanoaice@users.noreply.github.com>
2021-04-04 12:12:46 +0800

    refactor(lib): switch to preact (alias)

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [reactRefresh()],
+	resolve: {
+		alias: [
+			{
+				find: 'react',
+				replacement: 'preact/compat',
+			},
+			{
+				find: 'react-dom',
+				replacement: 'preact/compat',
+			},
+		],
+	},
 })
